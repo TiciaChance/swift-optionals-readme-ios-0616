2d3a765c678bf39a64954ec0698e764bb8a1a3a7
TiciaChance <laticia.chance@flatironschool.com>
2016-07-10 15:28:47 -0400

    hmmmm

--- Pets.playground/Contents.swift
+++ Pets.playground/Contents.swift
@@ -1,3 +1,6 @@
+// deleted and started from scratch
+
+
 var petName: String?
 petName = "Scooter 🐢"
 print(petName)
@@ -8,10 +11,14 @@ if petName != nil {
     print("My friend doesn't have a pet 😞")
 }
 
+// this new constant overrides the optinal petName, but only in the if statement
 if let petName = petName {
+    print("My friend's pet is \(petName)")
     print("My friend's pet is \(petName)")
 }
 
+
+// this gives me the option of using both the option and non-optional values
 if let friendsPet = petName {
     print("My friend's pet is \(friendsPet)")
     print("petName is still an Optional! \(petName)")
